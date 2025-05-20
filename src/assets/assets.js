3c2c009336347eae9262134aab04819cba3f75c0
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-05-20 12:07:34 +0800

    prj

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -12,6 +12,30 @@ import founder from './founder.jpeg'
 import mm from './mm.jpg'
 import com_logo from './com_logo.png'
 import herosection1 from './herosection1.jpg'
+import prj1 from './prj1.png'
+import prj2 from './prj2.jpg'
+import prj3 from './prj3.jpg'
+import prj4 from './prj4.jpg'
+import prj5 from './prj5.jpg'
+import prj6 from './prj6.jpg'
+import prj7 from './prj7.jpg'
+import prj8 from './prj8.jpg'
+import prj9 from './prj9.jpg'
+import prj10 from './prj10.jpg'
+import prj11 from './prj11.jpg'
+import prj12 from './prj12.jpg'
+import prj13 from './prj13.jpg'
+import prj14 from './prj14.jpg'
+import prj15 from './prj15.jpg'
+import prj16 from './prj16.jpg'
+import prj17 from './prj17.jpg'
+import prj18 from './prj18.jpg'
+import prj19 from './prj19.jpg'
+import prj20 from './prj20.jpg'
+import prj21 from './prj21.jpg'
+
+
+
  
 
 
@@ -34,7 +58,26 @@ export const assets = {
     mm,
     com_logo,
     herosection1,
-    
-
+    prj1,
+    prj2,
+    prj3,
+    prj4,
+    prj5,
+    prj6,
+    prj7,
+    prj8,
+    prj9,
+    prj10,
+    prj11,
+    prj12,
+    prj13,
+    prj14,
+    prj15,
+    prj16,
+    prj17,
+    prj18,
+    prj19,
+    prj20,
+    prj21
 
 }
